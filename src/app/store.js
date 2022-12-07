41b51ab177af32271f7138c43bb2f9120868d74c
AwesemoTowa <Deankurumi@gmail.com>
2022-12-07 17:18:11 +0600

    Free trash

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,3 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 
-export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
-});
+export const store = configureStore({});
